cfb6fb551acd4fc9df8b62be4767f300f73e6c88
Cafe137 <xcafe137@gmail.com>
2025-10-15 14:17:17 +0200

    feat: transform into a component library

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,48 @@
-import { createRoot } from 'react-dom/client'
-import { MultichainWidget } from './MultichainWidget'
+import { darkTheme, getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import '@rainbow-me/rainbowkit/styles.css'
+import { convertViemChainToRelayChain, createClient, MAINNET_RELAY_API } from '@relayprotocol/relay-sdk'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { MultichainLibrary, MultichainLibrarySettings } from '@upcoming/multichain-library'
+import { Objects } from 'cafe-utility'
+import { arbitrum, base, gnosis, mainnet, optimism, polygon } from 'viem/chains'
+import { WagmiProvider } from 'wagmi'
+import { getDefaultHooks, MultichainHooks } from './MultichainHooks'
+import { getDefaultMultichainTheme, MultichainTheme } from './MultichainTheme'
+import './MultichainWidget.css'
+import { Router } from './Router'
 
-const rootElement = document.getElementById('root')
-if (rootElement) {
-    const root = createRoot(rootElement)
-    root.render(<MultichainWidget />)
+const config = getDefaultConfig({
+    appName: 'Multichain Library',
+    projectId: '5119e426ef93d637395e119c5169ad79',
+    chains: [mainnet, polygon, optimism, arbitrum, base, gnosis],
+    ssr: false
+})
+
+const queryClient = new QueryClient()
+
+interface Props {
+    theme?: Partial<MultichainTheme>
+    hooks?: Partial<MultichainHooks>
+    settings?: Partial<MultichainLibrarySettings>
+}
+
+createClient({
+    baseApiUrl: MAINNET_RELAY_API,
+    chains: [convertViemChainToRelayChain(mainnet)]
+})
+
+export function MultichainWidget({ theme, hooks, settings }: Props) {
+    const mergedTheme = Objects.deepMerge2(getDefaultMultichainTheme(), theme || {})
+    const mergedHooks = Objects.deepMerge2(getDefaultHooks(), hooks || {})
+    const library = new MultichainLibrary(settings)
+
+    return (
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <RainbowKitProvider theme={darkTheme()}>
+                    <Router theme={mergedTheme} hooks={mergedHooks} library={library} />
+                </RainbowKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    )
 }
